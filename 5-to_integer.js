d4a51652c4a8e2ab5ffd050ac1666e09ab4dae90
Kelv-alt <noel.kel.vincent@gmail.com>
2025-05-22 10:17:09 +0100

    add to github

---
+++ 5-to_integer.js
@@ -0,0 +1,7 @@
+const arg = process.argv[2];
+const num = Number(arg);
+if (!isNaN(num) && arg !== undefined && arg !== null && arg.trim !== ""){
+    console.log(`My number: ${parseInt(num)}`)
+} else {
+    console.log("Not a number");
+}
